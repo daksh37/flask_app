425810db6c16089eb3659b601bbc0d18758d2328
daksh37 <46132606+daksh37@users.noreply.github.com>
2020-03-04 21:54:11 +0530

    Update flask_jira.py

--- flask_jira.py
+++ flask_jira.py
@@ -1,23 +1,10 @@
 from flask import Flask
-from jira import JIRA
-username = 'vasundhara.chauhan@innovaccer.com'
-password = 'Innovaccer@123'
-jira = JIRA(basic_auth=(username, password), options = {'server': 'https://support.innovaccer.com/'})
-projects = jira.projects()
+
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def helloIndex():
-    #print(projects)
-    #jra = jira.project('MER')
-    #print(jra.name)                
-    #print(jra.lead.displayName)
-    '''key='INT'
-    jira.create_project(key, name='INTEGER', assignee='daksh.singhal@innovaccer.com', type='Software', 
-    	template_name=None)'''
-    data = request.get_json()
-    print(data)
     return "hello"
 
 app.run()
