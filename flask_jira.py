d718db3fefa503eeb6d296612f1b7db1969e03f7
daksh37 <46132606+daksh37@users.noreply.github.com>
2020-03-04 22:01:39 +0530

    Update flask_jira.py

--- flask_jira.py
+++ flask_jira.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
-
 app = Flask(__name__)
 
 @app.route('/')
-def helloIndex():
-    return "hello"
+def hello_world():
+    return 'Hello World!'
 
-app.run()
+if __name__ == '__main__':
+    app.run()
